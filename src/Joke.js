f18d2dfcefd9bc69185427fe68c99af7bd0ca46d
Your Name <you@example.com>
2022-08-15 16:05:05 -0400

    joke js

--- src/Joke.js
+++ src/Joke.js
@@ -1,27 +1,75 @@
-import React from "react";
+import React , { Component } from "react";
 import "./Joke.css";
 
-function Joke({ vote, votes, text, id }) {
-  const upVote = () => vote(id, +1);
-  const downVote = () => vote(id, -1);
+class Joke extends Component{
+  constructor(props){
+    super(props);
+    this.upVote =this.upvote.bind(this);
+    this.downVote = this.downVote.bind(this);
+    this.toggleLock = this.toggleLock.bibd(this);
+  }
 
-  return (
-    <div className="Joke">
-      <div className="Joke-votearea">
-        <button onClick={upVote}>
-          <i className="fas fa-thumbs-up" />
-        </button>
+  upVote(){
+    this.props.vote(this.props.id, +1);
+  }
 
-        <button onClick={downVote}>
-          <i className="fas fa-thumbs-down" />
-        </button>
+  downVote(){
+    this.props.vote(this.props.id, -1);
+  }
 
-        {votes}
+  toggleLock(){
+    this.props.toggleLock(this.props.id);
+  }
+
+  render(){
+    return(
+      <div className = {`Joke ${this.props.locked ? "Joke-locked" : ""}`}>
+        <div className = "Joke-votearea">
+          <button onClick={this.upVote}>
+            <i className = "fas fa-thumbs-up" />
+          </button>
+
+          <button onClick={this.downVote}>
+            <i className = "fas fa-thumbs-down" />
+          </button>
+
+          <button onClick = {this.toggleLock}>
+            <i className = {`fas ${this.props.locked ? "fa-unlock" : "fa-lock"}`} />
+          </button>
+
+          {this.props.votes}
+
+          <div className = "Joke-text">{this.props.text}</div>
+
+        </div>
       </div>
+    );
+  }
 
-      <div className="Joke-text">{text}</div>
-    </div>
-  );
 }
+
+// function Joke({ vote, votes, text, id }) {
+//   const upVote = () => vote(id, +1);
+//   const downVote = () => vote(id, -1);
+
+//   return (
+//     <div className="Joke">
+//       <div className="Joke-votearea">
+//         <button onClick={upVote}>
+//           <i className="fas fa-thumbs-up" />
+//         </button>
+
+//         <button onClick={downVote}>
+//           <i className="fas fa-thumbs-down" />
+//         </button>
+
+//         {votes}
+//       </div>
+
+//       <div className="Joke-text">{text}</div>
+//     </div>
+//   );
+// }
+
 
 export default Joke;
